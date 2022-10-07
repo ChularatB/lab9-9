bc0e1e193be42f44c74936606ca9b0188e1e5cc7
ChularatB <91866956+ChularatB@users.noreply.github.com>
2022-10-07 20:17:44 +0700

    Update db9.php

--- db9.php
+++ db9.php
@@ -1,17 +1,5 @@
 <?php include "connect.php" ?>
 <html>
-
-<head>
-    <meta charset="utf-8">
-    <script>
-        function Edit(username) {
-            var ans = confirm("ต้องการแก้ไขชื่อ " + username);
-            if (ans == true)
-                document.location = "db9-1.php?username=" + username;
-        }
-    </script>
-</head>
-
 <body>
     <?php
     $stmt = $pdo->prepare("SELECT * FROM member");
@@ -21,7 +9,7 @@
         ที่อยู่ : <?=$row["address"]?><br>
         เบอร์โทร: <?=$row["mobile"]?><br>
         email: <?=$row["email"]?><br>
-        <a href='#' onclick='Edit("<?=$row["username"]?>")'>แก้ไข</a>
+        <a href='Edit.php?username("<?=$row["username"]?>")'>แก้ไข</a>
         <hr>
     <?php endwhile ?>
 </body>
